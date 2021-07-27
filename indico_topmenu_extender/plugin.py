19ed36933f387435694c4288964d596763976a36
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2021-07-27 16:44:20 +0200

    Initial commit

---
+++ indico_topmenu_extender/plugin.py
@@ -0,0 +1,57 @@
+from indico.core import signals
+from indico.core.plugins import IndicoPlugin
+from indico.web.menu import TopMenuItem
+
+
+
+class 
+
+
+from __future__ import unicode_literals
+
+from flask_pluginengine import render_plugin_template
+from wtforms.fields.core import StringField
+from wtforms.fields.html5 import URLField
+from wtforms.validators import DataRequired
+
+from indico.core.plugins import IndicoPlugin
+from indico.web.forms.base import IndicoForm
+from indico.web.views import WPBase
+
+from indico.web.forms.fields import MultipleItemsField, OverrideMultipleItemsField, PrincipalListField
+
+
+from indico_ursh import _
+from indico_ursh.blueprint import blueprint
+from indico_ursh.util import is_configured
+
+
+MENU_FIELDS= [{'id': 'menu_key', 'caption': _("Menu key"), 'required': True},
+              {'id': 'menu_title', 'caption': _("Menu title"), 'required': True},
+              {'id': 'menu_url', 'caption': _("Menu url"), 'required': True},
+              {'id': 'menu_weight', 'caption': _("Menu weight"), 'required': False},
+              {'id': 'menu_key', 'caption': _("Menu key"), 'required': True},
+              ]
+
+class SettingsForm(IndicoForm):
+    menu_entries = MultipleItemsField(_('Menu entries'), fields=MENU_FIELDS, unique_field='menu_key')
+
+class TopMenuExtenderPlugin(IndicoPlugin):
+    """Custom Top Menu Links
+
+    Add a new top menu links.
+    """
+
+    configurable = True
+    settings_form = SettingsForm
+    default_settings = {
+            'menu_entries': [],
+    }
+
+    def init(self):
+        super().init()
+        self.connect(signals.menu.items, self._extend_top_menu, sender='top-menu')
+
+    def _extend_top_menu(self, sender, **kwargs):
+        for entry in  self.settings.get('menu_entries'):
+            yield TopMenuItem(entry['menu_key'], entry['menu_title'], entry['menu_url'], weight=entry.get(menu_url, 95))
---
+++ setup.py
@@ -0,0 +1,37 @@
+# This file is part of the CERN Indico plugins.
+# Copyright (C) 2014 - 2018 CERN
+#
+# The CERN Indico plugins are free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
+from __future__ import unicode_literals
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='indico-topmenu-extender',
+    license='MIT',
+    author='MLZ indico team',
+    author_email='bjoern.pedersen@frm2.tum.de',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    use_scm_version={"write_to":"indico_topmenu_extener/version.py",
+                     "local_scheme":"node-and-timestamp"},
+    setup_requires = ["setuptools>=39", "setuptools_scm[toml]>=3.4"],
+    install_requires=[
+        'indico>=3'
+    ],
+    entry_points={
+        'indico.plugins': {'topmenu_extender=indico_topmenu_extender.plugin:TopMenuExtenderPlugin',
+                          }
+    },
+    classifiers=[
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
